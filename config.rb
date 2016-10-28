657af6e92c108cd0c8eb93d7b07ee663e87e6aa1
ShiraishiKakuya <taketori_kakuou7616@hotmail.co.jp>
2016-10-28 14:47:27 +0900

    initial commit

---
+++ config.rb
@@ -0,0 +1,16 @@
+
+
+#  Created by 白石覚也 on 2016/10/11.
+
+
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :expanded
+
+color_output = false
